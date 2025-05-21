dda4a78adf02c8beb0e3b966a7cbf27a5c4f9260
UriahVictorious <uriahvictorious@gmail.com>
2025-05-20 21:01:33 -0400

    Update 5-nat.tf

--- 5-nat.tf
+++ 5-nat.tf
@@ -1,1 +1,50 @@
+ # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_router_nat
+resource "google_compute_router_nat" "io-nat" {
+  name   = "io-nat"
+  router = google_compute_router.io-router.name
+  region = "us-central1"
+
+  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+  nat_ip_allocate_option             = "MANUAL_ONLY"
+
+  subnetwork {
+    name                    = google_compute_subnetwork.catchup-io1.name
+    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+  }
+
+  nat_ips = [google_compute_address.io-nat.self_link]
+}
+ 
+
+resource "google_compute_router_nat" "to-nat" {
+  name   = "to-nat"
+  router = google_compute_router.to-router.name
+  region = "asia-southeast1"
+
+  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+  nat_ip_allocate_option             = "MANUAL_ONLY"
+
+  subnetwork {
+    name                    = google_compute_subnetwork.catchup-to1.name
+    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+  }
+
+  nat_ips = [google_compute_address.to-nat.self_link]
+}
+
+resource "google_compute_router_nat" "br-nat" {
+  name   = "br-nat"
+  router = google_compute_router.br-router.name
+  region = "southamerica-east1"
+
+  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+  nat_ip_allocate_option             = "MANUAL_ONLY"
+
+  subnetwork {
+    name                    = google_compute_subnetwork.catchup-br1.name
+    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+  }
+
+  nat_ips = [google_compute_address.br-nat.self_link]
+}
 
