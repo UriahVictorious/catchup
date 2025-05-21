cceb3cb62b71541ecef527e867fa9bc9ed34f650
UriahVictorious <uriahvictorious@gmail.com>
2025-05-20 20:57:30 -0400

    Update 3-subnets.tf

--- 3-subnets.tf
+++ 3-subnets.tf
@@ -1,7 +1,1 @@
- resource "google_compute_subnetwork" "hqinternal" {
-  name                     = "hqinternal"
-  ip_cidr_range            = "10.97.255.0/24"
-  region                   = "us-central1"
-  network                  = google_compute_network.main.id
-  private_ip_google_access = true
-}
+
